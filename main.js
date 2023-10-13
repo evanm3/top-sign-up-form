44ac7957765d8f1c3bc57a8b4e33ba87fde44a04
evanm3 <132603114+evanm3@users.noreply.github.com>
2023-10-13 09:15:58 -0600

    clean up code

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-const errorText = document.querySelector('.error-text');
+    const errorText = document.querySelector('.error-text');
     const password = document.querySelector('#password');
     const passwordConfirm = document.querySelector('#password-confirm');
     const errorTextTwo = document.querySelector('#password-error-text-two');
+    const firstName = document.querySelector('#first-name');
 
     password.addEventListener("input", passwordValidation);
     passwordConfirm.addEventListener("input", passwordMatch);
-
-    errorText.textContent = "add this";
-
-   
+    firstName.addEventListener("input", blankChecker);
 
     function passwordValidation(event){
-        let greetings = "hello";
-        errorText.textContent = "${greetings}";
-        console.log(greetings);
-        const pwValue = password.value;
+        if (password.value === ''){
+            errorText.textContent = '';
+        }
+        else if (password.checkValidity() === false) {
+            errorText.textContent = `*must contain one capital,
+        one number and over 8 characters in length`;
+        } else {
+            errorText.textContent = '';
+        }
     }
     
     function passwordMatch(event){
         if (passwordConfirm.value !== password.value) {
-            errorTextTwo.textContent = "Passwords do not match";
+            errorTextTwo.textContent = "*Passwords do not match";
         }
         if (passwordConfirm.value === password.value) {
-            errorTextTwo.textContent = "match";
+            errorTextTwo.textContent = "";
         }
     }
